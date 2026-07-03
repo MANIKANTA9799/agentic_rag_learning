5e440812fcbe0bd1fba045de1f4f00236b4d0358
Manikanta <mytab123yy@gmail.com>
2026-07-03 20:08:13 +0530

    done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 load_dotenv()
-
+from graph.graph import app
 if __name__ =='__main__':
-    print("Heello rag")
+   print("Hello Advanced RAG")
+   print(app.invoke(input={"question": "what is agent memory?"}))#type:ignore 
