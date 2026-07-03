e80e8846010d657087ca04a5643e2180d2d38dac
Manikanta <mytab123yy@gmail.com>
2026-07-03 15:18:15 +0530

    retriver and grader done

--- graph/chain/tests/test_chains.py
+++
@@ -1,3 +0,0 @@
-#whenever we use pytest it only checks for files 
-# starting and having test as first name 
-
---
+++ graph/chains/tests/test_chains.py
@@ -0,0 +1,26 @@
+#whenever we use pytest it only checks for files 
+# starting and having test as first name 
+
+from dotenv import load_dotenv
+load_dotenv()
+from graph.chains.retreival_grader import GradeDocuments,retrieval_grader
+from ingestion import retriever
+
+def test_retrival_answer_yes()->None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+    res:GradeDocuments = retrieval_grader.invoke(
+        {"question":question,"document":doc_txt}
+    )#type:ignore 
+    assert res.binary_score == "yes"
+
+
+def test_retrival_grader_no():
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+    res:GradeDocuments = retrieval_grader.invoke(
+        {"question":"how to make pizza","document":doc_txt}
+    )#type:ignore 
+    assert res.binary_score == "no"
